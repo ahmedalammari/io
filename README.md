d162dc514377df8ac862cac8e2e173913d2889a7
ahmedalammari <104811801+ahmedalammari@users.noreply.github.com>
2022-05-03 08:58:43 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# TikTok-DL-Bot
+---
+
+Telegram Bot untuk Mengunduh video/audio Tiktok.
+
+## Demo Bot
+
+<a href="http://t.me/AnsakuTikTokDLBot"><img src="https://img.shields.io/badge/Telegram_Bot-2cb6e0?style=for-the-badge&logo=telegram&logoColor=white"></a>
+
+## Fitur:
+
+➪ Download Video TikTok tanpa Watermark
+
+➪ Download Video TikTok dengan Watermark
+
+➪ Download Audio TikTok
+
+## Deploy
+
+[![Heroku Deploy](https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white)](https://heroku.com/deploy?template=https://github.com/Ansaku/TikTok-DL-Bot)
+
+## Telegram Support:
+
+[![Channel](https://img.shields.io/badge/TG-Channel-30302f?style=flat&logo=telegram)](https://t.me/ansakubotchannel)
+[![Group](https://img.shields.io/badge/TG-Group-30302f?style=flat&logo=telegram)](https://t.me/ansakubotgrup)
+
+## Credits, and Thanks to
+
+* [Dan Tès](https://t.me/haskell) for his [Pyrogram Library](https://github.com/pyrogram/pyrogram)
+* [@hazaku-rei (Reiyuura)](https://github.com/hazaku-rei) for his [Free Rest API](https://api.reiyuura.me)
+
+## LICENSE
+- GPLv3
---
+++ bot.py
@@ -0,0 +1,202 @@
+import json, requests, os, shlex, asyncio, uuid, shutil
+from typing import Tuple
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+# Configs
+API_HASH = os.environ['cbe3dac12f4c9f0262b4ec619c117687']
+APP_ID = int(os.environ['10876271'])
+BOT_TOKEN = os.environ['5351112968:AAGCDCfOimr9dDmyJm4LmBQqky1jQvKhY2o']
+downloads = './downloads/{}/'
+
+# Helper
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+  args = shlex.split(cmd)
+  process = await asyncio.create_subprocess_exec(
+      *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+  )
+  stdout, stderr = await process.communicate()
+  return (
+      stdout.decode("utf-8", "replace").strip(),
+      stderr.decode("utf-8", "replace").strip(),
+      process.returncode,
+      process.pid,
+  )
+
+# Button
+START_TEXT = """
+Hai {}, Saya adalah TikTok-DL-Bot. Anda dapat mengunduh video/audio TikTok menggunakan bot ini. Kirimkan saya tautan tiktok yang ingin di unduh.
+
+Dibuat oleh @AnkiSatya
+"""
+HELP_TEXT = """
+- Kirimkan saya tautan tiktok
+- Pilih opsi yang anda inginkan
+- Saya akan mengunduhnya
+- Dan saya akan mengirim video/audio sesuai yang anda kirim
+
+Dibuat oleh @AnkiSatya
+"""
+ABOUT_TEXT = """
+- **Bot :** `TikTok DL Bot`
+- **Pembuat :** [𝔸𝕟𝕜𝕚 𝕊𝕒𝕥𝕪𝕒](https://t.me/AnkiSatya)
+- **Channel :** [𝕬𝖓𝖘𝖆𝖐𝖚 𝕭𝖔𝖙 𝕮𝖍𝖆𝖓𝖓𝖊𝖑](https://t.me/ansakubotchannel)
+- **Repo :** [Github](https://github.com/Ansaku/TikTok-DL-Bot)
+- **Donasi :** [Saweria](https://saweria.co/ansaku)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://t.me/ansakubotchannel'),
+        InlineKeyboardButton('Author', url='https://t.me/AnkiSatya')
+        ],[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+DL_BUTTONS=[
+    [
+        InlineKeyboardButton('No Watermark', callback_data='nowm'),
+        InlineKeyboardButton('Watermark', callback_data='wm'),
+    ],
+    [InlineKeyboardButton('Audio', callback_data='audio')],
+]
+
+
+# Running bot
+xbot = Client('TikTok-DL-Bot', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+
+# Start
+@xbot.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+@xbot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
+    )
+
+# Downloader for tiktok
+@xbot.on_message(filters.regex(pattern='.*http.*') & filters.private)
+async def _tiktok(bot, update):
+  url = update.text
+  session = requests.Session()
+  resp = session.head(url, allow_redirects=True)
+  if not 'tiktok.com' in resp.url:
+    return
+  await update.reply('Pilih opsi di bawah ini', True, reply_markup=InlineKeyboardMarkup(DL_BUTTONS))
+
+# Callbacks
+@xbot.on_callback_query()
+async def _callbacks(bot, cb: CallbackQuery):
+  if cb.data == 'nowm':
+    dirs = downloads.format(uuid.uuid4().hex)
+    os.makedirs(dirs)
+    cbb = cb
+    update = cbb.message.reply_to_message
+    await cb.message.delete()
+    url = update.text
+    session = requests.Session()
+    resp = session.head(url, allow_redirects=True)
+    if '?' in resp.url:
+      tt = resp.url.split('?', 1)[0]
+    else:
+      tt = resp.url
+    ttid = dirs+tt.split('/')[-1]
+    r = requests.get('https://api.reiyuura.me/api/dl/tiktok?url='+tt)
+    result = r.text
+    rs = json.loads(result)
+    link = rs['result']['nowm']
+    resp = session.head(link, allow_redirects=True)
+    r = requests.get(resp.url, allow_redirects=True)
+    open(f'{ttid}.mp4', 'wb').write(r.content)
+    await bot.send_video(update.chat.id, f'{ttid}.mp4',)
+    shutil.rmtree(dirs)
+  elif cb.data == 'wm':
+    dirs = downloads.format(uuid.uuid4().hex)
+    os.makedirs(dirs)
+    cbb = cb
+    update = cbb.message.reply_to_message
+    await cb.message.delete()
+    url = update.text
+    session = requests.Session()
+    resp = session.head(url, allow_redirects=True)
+    if '?' in resp.url:
+      tt = resp.url.split('?', 1)[0]
+    else:
+      tt = resp.url
+    ttid = dirs+tt.split('/')[-1]
+    r = requests.get('https://api.reiyuura.me/api/dl/tiktok?url='+tt)
+    result = r.text
+    rs = json.loads(result)
+    link = rs['result']['wm']
+    resp = session.head(link, allow_redirects=True)
+    r = requests.get(resp.url, allow_redirects=True)
+    open(f'{ttid}.mp4', 'wb').write(r.content)
+    await bot.send_video(update.chat.id, f'{ttid}.mp4',)
+    shutil.rmtree(dirs)
+  elif cb.data == 'audio':
+    dirs = downloads.format(uuid.uuid4().hex)
+    os.makedirs(dirs)
+    cbb = cb
+    update = cbb.message.reply_to_message
+    await cb.message.delete()
+    url = update.text
+    session = requests.Session()
+    resp = session.head(url, allow_redirects=True)
+    if '?' in resp.url:
+      tt = resp.url.split('?', 1)[0]
+    else:
+      tt = resp.url
+    ttid = dirs+tt.split('/')[-1]
+    r = requests.get('https://api.reiyuura.me/api/dl/tiktok?url='+tt)
+    result = r.text
+    rs = json.loads(result)
+    link = rs['result']['wm']
+    resp = session.head(link, allow_redirects=True)
+    r = requests.get(resp.url, allow_redirects=True)
+    open(f'{ttid}.mp4', 'wb').write(r.content)
+    cmd = f'ffmpeg -i "{ttid}.mp4" -vn -ar 44100 -ac 2 -ab 192 -f mp3 "{ttid}.mp3"'
+    await run_cmd(cmd)
+    await bot.send_audio(update.chat.id, f'{ttid}.mp3',)
+    shutil.rmtree(dirs)
+
+xbot.run()
